16d130cb65e8bd136295a0d6496c4062636bdeaf
RaoulChartreuse <laurent.morin.morin@gmail.com>
2019-07-05 11:55:00 +0200

    Add permission in AndroidManifest

--- app/src/main/java/fr/lmorin/flip2snooze/wakeupActivity.java
+++ app/src/main/java/fr/lmorin/flip2snooze/wakeupActivity.java
@@ -14,11 +14,11 @@ import android.media.MediaPlayer;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -31,7 +31,6 @@ import android.widget.Toast;
 import static java.lang.Math.cos;
 import static java.lang.Math.exp;
 import static java.lang.Math.sqrt;
-import static java.security.AccessController.getContext;
 
 // This activity is a display activity
 // No deleting of alarm !
@@ -45,6 +44,55 @@ public class wakeupActivity extends AppCompatActivity {
     long tStart;
     Intent snoozeIntent;
     AlarmRecord alarm;
+    final SensorEventListener mSensorEventListener = new SensorEventListener() {
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // Rien a faire en cas de changement de précision
+        }
+
+        public void onSensorChanged(SensorEvent sensorEvent) {
+            long tNow = SystemClock.elapsedRealtime();
+            double dt = (tNow - tStart) * .001;
+            final double t_flip = 1.5; //TODO better be in preference setting (to be implemented)
+            tStart = tNow;
+
+            final float alpha = (float) exp(-3 * dt / t_flip);
+            // Isolate the force of gravity with the low-pass filter.
+            gravity[0] = alpha * gravity[0] - (1 - alpha) * sensorEvent.values[0];
+            gravity[1] = alpha * gravity[1] - (1 - alpha) * sensorEvent.values[1];
+            gravity[2] = alpha * gravity[2] - (1 - alpha) * sensorEvent.values[2];
+
+
+        }
+    };
+    private boolean stopNow = false;
+
+
+    private final String FLIP_DURATION = "flip duration";
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            int duration = msg.getData().getInt(FLIP_DURATION);
+            ringtone.stop();
+            mp.start();
+
+            String text = "flipped!";
+
+            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+            toast.show();
+
+            snoozeIntent = new Intent(getBaseContext(), wakeupActivity.class);
+            snoozeIntent.setData(Uri.parse("Snooze://" + alarm.mId));
+            snoozeIntent.putExtra("alarmId", alarm.mId.toString());
+            PendingIntent displayIntent = PendingIntent.getActivity(getBaseContext(), 0, snoozeIntent, 0);
+
+            long tNow = SystemClock.elapsedRealtime();
+            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+            long tSnooze = 2; //min//TODO set in preference
+            alarmManager.set(AlarmManager.RTC_WAKEUP, tNow + (tSnooze * 60 * 1000), displayIntent);
+            finish();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,8 +108,8 @@ public class wakeupActivity extends AppCompatActivity {
         View vAlarm = getLayoutInflater().inflate( R.layout.list_cell, null);
         LinearLayout layout = findViewById(R.id.wakeupLayout);
         layout.addView(vAlarm);
-        TextView name = ((TextView) findViewById(R.id.name));
-        TextView description = ((TextView) findViewById(R.id.jour));
+        TextView name = findViewById(R.id.name);
+        TextView description = findViewById(R.id.jour);
         TextView heure = findViewById(R.id.heure);
         ImageButton deleteButton = findViewById(R.id.deleteButton);
         Switch IsActive = findViewById(R.id.switch1);
@@ -93,6 +141,7 @@ public class wakeupActivity extends AppCompatActivity {
                 AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
                 alarm.mIsActive = false;
                 alarmManager.cancel(displayIntent);
+                stopNow = true;
                 finish();
             }
         });
@@ -107,34 +156,6 @@ public class wakeupActivity extends AppCompatActivity {
         start_sequence();
 
     }
-
-
-    private final String FLIP_DURATION="flip duration";
-    @SuppressLint("HandlerLeak")
-    Handler handler = new Handler() {
-        @Override
-        public void handleMessage(Message msg) {
-            int duration = msg.getData().getInt(FLIP_DURATION);
-            ringtone.stop();
-            mp.start();
-
-            String text = "flipped!";
-
-            Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
-            toast.show();
-
-            snoozeIntent = new Intent(getBaseContext(), wakeupActivity.class);
-            snoozeIntent.setData(Uri.parse("Snooze://"+alarm.mId));
-            snoozeIntent.putExtra("alarmId", alarm.mId.toString());
-            PendingIntent displayIntent = PendingIntent.getActivity(getBaseContext(), 0, snoozeIntent,0);
-
-            long tNow = SystemClock.elapsedRealtime();
-            AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
-            long tSnooze= 2; //min//TODO set in preference
-            alarmManager.set(AlarmManager.RTC_WAKEUP, tNow+(tSnooze*60*1000), displayIntent);
-            finish();
-        }
-    };
 
     private void start_sequence() {
         Thread background = new Thread(new Runnable() {
@@ -167,6 +188,7 @@ public class wakeupActivity extends AppCompatActivity {
                     while (!test()) {
                         Thread.sleep(200);
                         Log.v(TAG, String.format("u_ref = %f, %f, %f", ux, uy, uz ));
+                        if (stopNow) throw new Throwable();
                     }
                     long tEnd = SystemClock.elapsedRealtime();
 
@@ -175,34 +197,13 @@ public class wakeupActivity extends AppCompatActivity {
                     myMessage.setData(messageBundle);
                     handler.sendMessage(myMessage);
                 }catch (Throwable t) {
-                    // gérer l'exception et arrêter le traitement ??
+                    // Finier le thread est suffisant
                 }
             }
 
         });
         background.start();
     }
-
-    final SensorEventListener mSensorEventListener = new SensorEventListener() {
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-            // Rien a faire en cas de changement de précision
-        }
-
-        public void onSensorChanged(SensorEvent sensorEvent) {
-            long tNow = SystemClock.elapsedRealtime();
-            double dt = (tNow-tStart)*.001;
-            final double t_flip = 1.5; //TODO better be in preference setting (to be implemented)
-            tStart= tNow;
-
-            final float alpha = (float) exp(-3*dt/t_flip);;
-            // Isolate the force of gravity with the low-pass filter.
-            gravity[0] = alpha * gravity[0] - (1 - alpha) * sensorEvent.values[0];
-            gravity[1] = alpha * gravity[1] - (1 - alpha) * sensorEvent.values[1];
-            gravity[2] = alpha * gravity[2] - (1 - alpha) * sensorEvent.values[2];
-
-
-        }
-    };
 
 
     @Override
